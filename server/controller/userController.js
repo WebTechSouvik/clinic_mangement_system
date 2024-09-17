01e1823710866187bca1e11c93459011784dc22e
Srijan Giri <srijan_g.ece2020@msit.edu.in>
2024-09-18 00:04:55 +0530

    folder created

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,5 @@
 const { where } = require("sequelize");
-const { User } = require("../../models");
+const { User } = require("../models");
 const generateToken = require("../utils/token");
 
 const userRegister = async (req, res) => {
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./models");
-const app = require("./src/app.js");
+const app = require("./app.js");
 
 dotenv.config();
 
